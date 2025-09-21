b782e90341c0160d6a30e1dd37d4dc3b77ec07f1
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-21 10:38:40 -0500

    Enable Flatpak and update user password hash

--- hosts/common.nix
+++ hosts/common.nix
@@ -1,4 +1,4 @@
-# in hosts/common.nix
+# hosts/common.nix
 { pkgs, ... }:
 
 {
@@ -15,21 +15,34 @@
   # Enable the SSH daemon.
   services.sshd.enable = true;
 
+  # Enable Flatpak for applications not available in nixpkgs
+  services.flatpak.enable = true;
+
   # Define your user account.
   users.users.hbohlen = {
     isNormalUser = true;
     description = "Your Name";
     extraGroups = [ "wheel" "networkmanager" ];
-    hashedPassword = "$$6$g/OrnhCxwGMCBMYD$Xch2yusAyYKvAih6vk9AYE7GeeDGyaoc.J1s59TQ9JQA1PufdXRYtQ1l3UK9MsaF7oVFwqQKQyXov4oFb2xrH0";
+    # Replace this with your actual password hash
+    hashedPassword = "$6$your-password-hash-here";
   };
 
-  # List packages you want to install on all systems.
+  # List system-wide packages (available to all users)
   environment.systemPackages = with pkgs; [
     git
     wget
     vim
     podman
+    vivaldi  # Web browser - system-wide installation
   ];
+
+  # 1Password setup - required for the GUI to work properly
+  security.wrappers.1password = {
+    owner = "root";
+    group = "root";
+    capabilities = "cap_ipc_lock+ep";
+    source = "${pkgs._1password-gui}/bin/1password";
+  };
 
   # Basic Nix settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];

f7089af9291087ad13e0ece53e176adc6906a761
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-21 03:35:21 +0000

    Update hashed password for user hbohlen

--- hosts/common.nix
+++ hosts/common.nix
@@ -20,7 +20,7 @@
     isNormalUser = true;
     description = "Your Name";
     extraGroups = [ "wheel" "networkmanager" ];
-    hashedPassword = "$6$1321$....";
+    hashedPassword = "$$6$g/OrnhCxwGMCBMYD$Xch2yusAyYKvAih6vk9AYE7GeeDGyaoc.J1s59TQ9JQA1PufdXRYtQ1l3UK9MsaF7oVFwqQKQyXov4oFb2xrH0";
   };
 
   # List packages you want to install on all systems.
@@ -34,4 +34,5 @@
   # Basic Nix settings
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
   nix.settings.auto-optimise-store = true;
+  system.stateVersion = "25.05";
 }

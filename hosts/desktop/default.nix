6a68da20744cda90842ead421f586085c6d7a6b5
Hayden Bohlen <bohlenhayden@gmail.com>
2025-09-21 03:35:36 +0000

    Update Nix configuration for boot and services

--- hosts/desktop/default.nix
+++ hosts/desktop/default.nix
@@ -5,15 +5,22 @@
     ./hardware-configuration.nix # This will be generated during installation
     ../common.nix
   ];
-
+  boot.loader.systemd-boot.enable = true;
+  
+  fileSystems."/persist".neededForBoot = true;
   networking.hostName = "nix-desktop";
+  hardware.nvidia.open = false;
 
   services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.gnome-remote-desktop.enable = false;
 
-  sound.enable = true;
+
   hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire.enable = false;
+  services.pipewire.pulse.enable = false;
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
